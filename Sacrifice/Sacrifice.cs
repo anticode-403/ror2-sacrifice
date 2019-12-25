2e4b90fd3299e495ec01f14fcda12498ef55af6a
Magnus <magnus3432@gmail.com>
2019-12-25 09:01:34 +0100

    Updated libs and fix for Hidden Realms Content Update

--- Sacrifice/Sacrifice.cs
+++ Sacrifice/Sacrifice.cs
@@ -435,7 +435,7 @@ namespace Sacrifice
           {
             if (directorCard.spawnCard.prefab.name == "Chest1") deathChestSpawnCard = directorCard;
             if (!ApplyConfigModifiers(directorCard)) continue;
-            directorCard.cost = Mathf.RoundToInt(directorCard.cost * interactableCostMultiplier);
+            directorCard.spawnCard.directorCreditCost = Mathf.RoundToInt(directorCard.cost * interactableCostMultiplier);
             weightedSelection.AddChoice(directorCard, directorCard.selectionWeight / num * category.selectionWeight);
           }
         }
